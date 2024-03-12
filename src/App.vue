21062ffd1c2cbf5610ee4588696023871f4e3faf
Lukáš Kus <kus.lukas@seznam.cz>
2024-03-12 23:00:30 +0100

    style: edit HelloWorld message with styling & paragraph

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,9 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <HelloWorld msg="Projekt Poké App funguje." />
+  <HelloWorld msg="Hello Světe!"/>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
