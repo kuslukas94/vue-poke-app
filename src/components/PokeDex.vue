49a241e51d40169b800db18902094da83afeb745
Lukáš Kus <kus.lukas@seznam.cz>
2024-03-15 21:52:17 +0100

    feat: PokeDex.vue with list of evolution of Pikachu

---
+++ src/components/PokeDex.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from 'vue'
+
+const pikachuevo = ref([{ evo: 'Pikachu' }, { evo: 'Raichu' }, { evo: 'Pichu'}])
+</script>
+
+<template>
+	<ul v-for="(item) in pikachuevo">
+  	{{ item.evo }}
+	</ul>
+</template>
